e448b489012e724fe9fdd47d6fb65bac66db3aea
Mansur <akmetov_m@iuca.kg>
2025-10-17 00:40:32 +0600

    forgot footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import CoursesList from "./pages/CoursesList";
 import CourseDetail from "./pages/CourseDetail";
@@ -10,12 +11,16 @@ const App = () => {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/courses" element={<CoursesList />} />
-        <Route path="/courses/:id" element={<CourseDetail />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-      </Routes>
+      <div style={{ minHeight: "calc(100vh - 160px)" }}>
+        {}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/courses" element={<CoursesList />} />
+          <Route path="/courses/:id" element={<CourseDetail />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </div>
+      <Footer />
     </Router>
   );
 };
